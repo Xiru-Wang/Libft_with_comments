93025971e59d58940df256b34f97a3a1cbf9a866
Xiru Wang <xiwang@c4b3c1.42berlin.de>
2023-05-11 20:10:36 +0200

    updated

---
+++ ft_strtrim.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xiwang <xiwang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 16:01:23 by xiwang            #+#    #+#             */
+/*   Updated: 2023/05/11 19:00:24 by xiwang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+size_t	ft_char_in_set(char c, char const *set)
+{
+	while (*set)
+	{
+		if (*set == c)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	i;
+	char	*ret;
+
+	start = 0;
+	end = strlen(s1);
+	//if (!s1 || !set)
+	//	return (0);
+	while (s1[start] && ft_char_in_set(s1[start], set))//check if start from 1st index
+		start++;//if yes, ++ to find where to start to trim
+	while (start < end && ft_char_in_set(s1[end - 1], set))// check from last index
+		end--;
+	ret = (char *)malloc(sizeof(char)*(end - start + 1));
+	if (ret == NULL)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		ret[i++] = s1[start++];
+	ret[i] = 0;
+	return (ret);
+}
+
+/*int	main(void)
+{
+	char	str1[] = "abcdefgcc";
+	char	str2[] = "ace";
+	//char	str3[] = "ac";
+	char	*trimmed;
+	// char	str4[] = "de";
+
+	trimmed = ft_strtrim(str1, str2);
+	printf("%s\n", trimmed);
+	free(trimmed);
+	//printf("%s\n", ft_strtrim(str1, str3));
+	//printf("%s\n", ft_strtrim(str1, str4));
+
+	return (0);
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: xiwang <xiwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 19:05:24 by xiwang            #+#    #+#             */
-/*   Updated: 2023/05/10 20:40:03 by xiwang           ###   ########.fr       */
+/*   Updated: 2023/05/11 16:00:55 by xiwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 //void (*del)(void *);
 
 #endif
